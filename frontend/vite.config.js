2ab2499ca51afd75c901ace1f272b71a976e08f1
guyleibovic12 <guyleibovic12@gmail.com>
2025-09-12 21:10:57 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// קובץ הגדרות של Vite
 export default defineConfig({
   plugins: [react()],
-  preview: { allowedHosts: true, host: '0.0.0.0', port: 10000 }
+  preview: {
+    allowedHosts: [
+      "sales-frontend-z2m7.onrender.com",  // ה־URL של ה-Render שלך
+    ],
+    host: '0.0.0.0',
+    port: 10000
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
