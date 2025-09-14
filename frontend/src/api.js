a702e6ad32efaf841a9fd62b85ad30f17ec9a940
guyleibovic12 <guyleibovic12@gmail.com>
2025-09-14 17:36:01 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sales-backend-6yh2.onrender.com", // הכתובת של ה-backend ב-Render
+  baseURL: "https://sales-backend-6yh2.onrender.com", // backend URL
 });
 
 export default api;
